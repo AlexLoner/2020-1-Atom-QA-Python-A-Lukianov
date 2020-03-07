8e18ecbc0e590912f703ae78b749f84e5d709b2f
Aleksandr Lukianov <lukyanov9567@gmail.com>
2020-03-07 19:05:13 +0300

    ДЗ_1 (#1)

---
+++ hw1/test_list.py
@@ -0,0 +1,43 @@
+''' Module for testing 5 properties of python List instance '''
+import pytest
+
+
+@pytest.fixture()
+def element():
+    """Return constant list for futher testing"""
+    return ['dfa', 190, [[10]], {0, 23, 1}]
+
+# ------------------------------------------------
+def test1(element):
+    """Check 'something' is a direct or inherited instance of list class"""
+    assert isinstance(element, list)
+
+# ------------------------------------------------
+class Test:
+    '''Class for test possible properties of list instance'''
+
+    # ------------------------------------------------
+    def test2(self, element):
+        """Check that lists can't be multiplied by non-int"""
+        with pytest.raises(TypeError):
+            assert element * element
+
+    # ------------------------------------------------
+    def test3(self, element):
+        """Check len method"""
+        assert len(element) >= 0
+
+    # ------------------------------------------------
+    def test4(self, element):
+        """Check mutable property"""
+        element[-1] = 100
+        assert element[-1] == 100
+
+# ------------------------------------------------
+attr_lst = ['copy', 'append', 'count', 'extend']
+
+
+@pytest.mark.parametrize("value", attr_lst)
+def test5(value, element):
+    """Check list attributes on element instance"""
+    assert hasattr(element, value)
